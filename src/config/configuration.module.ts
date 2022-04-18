7333a0a159e6aab980bde7af7d23d83d80136e45
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-04-18 18:36:39 -0300

    try again run deploy

--- src/config/configuration.module.ts
+++ src/config/configuration.module.ts
@@ -5,7 +5,7 @@ import { ConfigurationService } from "./configuration.service";
 @Module({
     exports: [ConfigurationService],
     imports: [ConfigModule.forRoot({
-        envFilePath: ['.env.development.local']
+        envFilePath: ['.env.development.local', '.env']
     })],
     providers: [ConfigurationService]
 })
